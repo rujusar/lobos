7210f974d5a0784816ffcb2ab261b0d0571a75a0
Juan Covas Medina <jcovas@autentia.com>
2017-04-05 00:32:57 +0200

    project libs config

--- app/src/main/java/com/rujusar/lobos/CastOptionsProvider.java
+++ app/src/main/java/com/rujusar/lobos/CastOptionsProvider.java
@@ -1,8 +1,27 @@
 package com.rujusar.lobos;
 
+import android.content.Context;
+
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import java.util.List;
+
 /**
  * Created by jcovas on 05/04/2017.
  */
 
-public class CastOptionsProvider {
+class CastOptionsProvider implements OptionsProvider {
+    @Override
+    public CastOptions getCastOptions(Context appContext) {
+        CastOptions castOptions = new CastOptions.Builder()
+                .setReceiverApplicationId(appContext.getString(R.string.app_id))
+                .build();
+        return castOptions;
+    }
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
 }
